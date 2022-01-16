665fb327714bf60c17074137099fc19e687b7cc9
l4blee <76159455+l4blee@users.noreply.github.com>
2022-01-16 19:53:14 +0700

    upd

--- gamemap.py
+++ gamemap.py
@@ -5,7 +5,6 @@ import pygame
 from enum import Enum
 
 
-
 class Textures(Enum):
     ALIASES = {
         '#': 'GRASS',
@@ -14,7 +13,7 @@ class Textures(Enum):
 
     GRASS = pygame.transform.scale(load_image('tiles/grass.png'),
                                    (config.TILE_SIZE, config.TILE_SIZE))
-    DIRT = pygame.transform.scale(load_image(f'tiles/dirt.png'),
+    DIRT = pygame.transform.scale(load_image('tiles/dirt.png'),
                                   (config.TILE_SIZE, config.TILE_SIZE))
 
 
@@ -42,11 +41,11 @@ class Chunk:
                       self.y * self.size * config.TILE_SIZE + tile.y * tile.size)
 
             if 0 - config.TILE_SIZE // 2 < coords[0] < config.SCREEN_SIZE[0] or\
-                 0 - config.TILE_SIZE // 2 < coords[1] < config.SCREEN_SIZE[1]:
+                    0 - config.TILE_SIZE // 2 < coords[1] < config.SCREEN_SIZE[1]:
                 screen.blit(
                     tile.texture.value,
                     (self.x * self.size * config.TILE_SIZE + tile.x * tile.size,
-                    self.y * self.size * config.TILE_SIZE + tile.y * tile.size)
+                     self.y * self.size * config.TILE_SIZE + tile.y * tile.size)
                 )
 
     def __repr__(self):
